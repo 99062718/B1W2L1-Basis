d4ca7da72381cbfdb66a20017412fe84af210ef2
Tycho Hartingsveld <99062718@mydavinci.nl>
2020-09-09 12:22:41 +0200

    push

---
+++ javascript.js
@@ -0,0 +1,15 @@
+/*
+Tycho Hartingsveld
+Software developer
+*/
+
+alert("Welkom op onze website. Voor verificatie dient uw 3 vragen te beantwoorden over uw naam en leeftijd.") // geeft pop-up dat gebruiker 3 vragen dient te beantwoorden
+
+document.write("Uw voornaam is: " + prompt("Wat is uw voornaam?")); // hier print het een stuk text met daarachter de var voornaam
+document.write("<br>"); // enter
+
+document.write("Uw achternaam is: " + prompt("Wat is uw achternaam?")); // hier print het een stuk text met daarachter de var achternaam
+document.write("<br>");// enter
+
+document.write("Uw leeftijd is: " + prompt("Wat is uw leeftijd?")); // hier print het een stuk text met daarachter de var leeftijd
+document.write("<br>"); // enter
